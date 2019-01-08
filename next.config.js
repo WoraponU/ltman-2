ffe8c9df0493a7437d6138f3dd87b87b13c59f81
worapon prathuangthip <woraponok@gmail.com>
2019-01-09 02:40:00 +0700

    query apollo ได้แล้ว

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const withSass = require('@zeit/next-sass')
 
 module.exports = withSass({
   publicRuntimeConfig: {
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    APOLLO_SERVER_URL: process.env.APOLLO_SERVER_URL
   },
 
   webpack(config, options) {
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,50 @@
 import React, { PureComponent } from 'react'
 import { Button } from 'reactstrap'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
 class Index extends PureComponent {
   render() {
     return (
       <div>
         <Button color="danger">Danger!</Button>
+        <Query query={query}>
+          {({ loading, error, data }) => {
+            if (loading) return 'Loading...'
+            if (error) return `Error! ${error.message}`
+            console.log(data)
+            return <div>eiei</div>
+          }}
+        </Query>
       </div>
     )
   }
 }
 
 export default Index
+
+const query = gql`
+  query {
+    feed(mode: v5_popular) {
+      count
+      data {
+        ... on FeedItemArticle {
+          article {
+            id
+            page {
+              profile {
+                name
+                photo {
+                  sizes(size: profile_thumb) {
+                    name
+                    src
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
